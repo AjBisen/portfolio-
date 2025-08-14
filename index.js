8eb0eb0ceb8143dc53b4b3a2afd4e42673baeb20
Ajay Bisen <145466159+AjBisen@users.noreply.github.com>
2025-08-14 10:23:23 +0530

    portfolio
    portfolio

---
+++ index.js
@@ -0,0 +1,29 @@
+// // Smooth Scroll for nav links
+// document.querySelectorAll('nav a').forEach(link => {
+//   link.addEventListener('click', function(e) {
+//     e.preventDefault();
+//     const section = document.querySelector(this.getAttribute('href'));
+//     if (section) {
+//       section.scrollIntoView({ behavior: 'smooth' });
+//     }
+//   });
+// });
+
+
+
+
+
+
+
+// document.getElementById('contactForm').addEventListener('submit', function(e) {
+//   e.preventDefault();
+//   alert('Thank you for your message!');
+// });
+
+
+
+
+// script.js
+// Aap yaha animations, button click, scroll effects add kar sakte ho
+
+console.log("Portfolio site loaded!");
